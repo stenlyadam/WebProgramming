175ad36183037150254c4d0ffb86ff1aa3d71e89
Rave The Rave <ryvenrobot@gmail.com>
2021-11-17 23:55:46 +0800

    Exercise 6 Commit

---
+++ my-first-app/src/pages/Dashboard/index.js
@@ -0,0 +1,10 @@
+import NavBar from "../../components/NavBar"
+
+export default function Dashboard() {
+    return (
+        <div>
+            <NavBar />
+            <h3>Dashboard Page</h3>
+        </div>
+    )
+}
